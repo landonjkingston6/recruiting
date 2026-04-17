20cd0a2e8d128de28168539fa04929e34b64c065
Bahrbaquer <christopherbahr@alpinedistrict.org>
2026-04-17 09:20:53 -0600

    update

--- js/script.js
+++ js/script.js
@@ -25,26 +25,70 @@
                                 const height = row[6] || '';
                                 const number = row[7] || '';
                                 const gradYear = row[8] || '';
+                                const weight = row[9] || '';
+                                const bench = row[10] || '';
+                                const squat = row[11] || '';
+                                const clean = row[12] || '';
+                                const fourty = row[13] || '';
+                                const hundred = row[14] || '';
+                                const shuttle = row[15] || '';
+                                const broadJ = row[16] || '';
+                                const vertJ = row[17] || '';
+                                const act = row[18] || '';
+                                const gpa = row[19] || '';
+                                const coachEmail = row[20] || '';
 
                                 const card = document.createElement('div');
                                 card.className = 'player-card';
                                 card.innerHTML = `
-                                    <div class="cardOverlay"></div>
-                                    <h5 class="number">${number}</h5>
-                                    <div class="player-card-grid">
-                                        <h3 class="lName">${L_Name}</h3>
-                                        <h5 class=fName>${F_Name}</h5>
-                                        <h5 class="gradYear">Class of ${gradYear}</h5>
-                                        <p class="position">Position:<span class="textAdjust"> ${position}</span></p>
-                                        <img class="playerBioImage" src="images/playerImages/${imageURL}">
-                                        <p class="offers">Offers:<span class="textAdjust"> ${offers}</span></p>
-                                        <a href="${hudl}" class="hudl" target="_blank">View Hudl</a>
-                                        <p class="height">Height:<span class="textAdjust"> ${height}</span></p>
-                                        <button class="playerButton">View Profile</button>
+                                    <div class="player-card-face front">
+                                        <div class="cardOverlay"></div>
+                                        <h5 class="number">${number}</h5>
+                                        <div class="player-card-grid">
+                                            <h3 class="lName">${L_Name}</h3>
+                                            <h5 class=fName>${F_Name}</h5>
+                                            <h5 class="gradYear">Class of ${gradYear}</h5>
+                                            <p class="position">Position:<span class="textAdjust"> ${position}</span></p>
+                                            <img class="playerBioImage" src="images/playerImages/${imageURL}">
+                                            <p class="offers">Offers:<span class="textAdjust"> ${offers}</span></p>
+                                            <a href="${hudl}" class="hudl" target="_blank">View Hudl</a>
+                                            <p class="height">Height:<span class="textAdjust"> ${height}</span></p>
+                                            <button type="button" class="playerButton viewProfileButton">View Profile</button>
+                                        </div>
                                     </div>
-                                   
+                                    <div class="player-card-face back">
+                                        <div class="back-content">
+                                            <h3 class="fullName">${F_Name} ${L_Name}</h3>
+                                            <p class="back-item height">Height:<span>${height}</span></p>
+                                            <p class="back-item weight">Weight:<span>${weight}</span></p>
+                                            <p class="back-item position">Position:<span>${position}</span></p>
+                                            <p class="back-item bench">Bench: ${bench}</p>
+                                            <p class="back-item squat">Squat: ${squat}</p>
+                                            <p class="back-item clean">Clean: ${clean}</p>
+                                            <p class="back-item fourty">40 Yard Dash: ${fourty}</p>
+                                            <p class="back-item hundred">100 meter: ${hundred}</p>
+                                            <p class="back-item shuttle">Shuttle: ${shuttle}</p>
+                                            <p class="back-item broadJ">Broad Jump: ${broadJ}</p>
+                                            <p class="back-item vertJ">Vertical Jump: ${vertJ}</p>
+                                            <p class="back-item act">ACT: ${act}</p>
+                                            <p class="back-item gpa">GPA: ${gpa}</p>
+                                            <p class="back-item coachEmail">Coach Contact: ${coachEmail}</p>
+                                            <button type="button" class="playerButton backButton">Back</button>
+                                        </div>
+                                    </div>
                                 `;
                                 playersDiv.appendChild(card);
+
+                                const viewProfileButton = card.querySelector('.viewProfileButton');
+                                const backButton = card.querySelector('.backButton');
+
+                                viewProfileButton.addEventListener('click', () => {
+                                    card.classList.add('flipped');
+                                });
+
+                                backButton.addEventListener('click', () => {
+                                    card.classList.remove('flipped');
+                                });
                             }
                         });
                     },
